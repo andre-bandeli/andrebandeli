7505ca2b717049c3e03329ab857100793208caa9
andre-bandeli <andre.lbandeli@gmail.com>
2023-04-04 15:19:13 -0300

    feat: npm router-dom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 
 import './App.css';
-import Contato from './components/contato/Contato';
-import Ferramentas from './components/ferramentas/Ferramentas';
-import Header from './components/header/Header';
-import Navbar from './components/Nav/Navbar'
-import Portfolio from './components/portfolio/Portfolio';
-import Sobre from './components/sobre/Sobre';
-import Certificados from './components/certificados/Certificados'
-import Footer from './components/footer/Footer';
-import Cards from './components/cards/Cards';
+import { Route, Routes } from "react-router-dom"
+import Index from './Index';
 
 function App() {
 
   
   return (
-    <div className="App">
-      <Navbar/>
-      <Header/>
-      <Cards/>
-      <Portfolio/>
-      <Sobre/>
-      <Certificados/>
-      <Ferramentas/>
-      <Contato/>
-      <Footer/>
-
-    </div>
+    <Routes>
+        <Route path="/" element={<Index />} />
+    </Routes>
   );
 }
 
